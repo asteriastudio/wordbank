2bb1c0af76bb4e48077ee8d01a559a668a378a9a
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2023-01-01 18:24:38 -0600

    Add sample phrases

---
+++ wordbank.js
@@ -0,0 +1,49 @@
+const GENERATED_NAME_SELECTOR = '#generated-name';
+
+const PREFIXES = [
+  'Emerald',
+  'Autumn',
+  'Velvet',
+  'Topaz',
+  'Amber',
+];
+
+const SUFFIXES = [
+  'House',
+  'Place',
+  'Atelier',
+  'Stories',
+  'Home',
+];
+
+function onGenerateClick(event) {
+  event.preventDefault();
+
+  const prefixIndex = Math.random(PREFIXES.length - 1)
+  const suffixIndex = Math.random(SUFFIXES.length - 1)
+
+  const prefix = selectWord(PREFIXES);
+  const suffix = selectWord(SUFFIXES);
+
+  const value = document.createElement('h1')
+  value.innerHTML = `${prefix} ${suffix}`;
+
+  document.querySelector(GENERATED_NAME_SELECTOR).replaceChildren(value);
+}
+
+function selectWord(list) {
+  const index = Math.floor(Math.random() * list.length);
+
+  return list[index];
+}
+
+function main() {
+  init();
+}
+
+function init() {
+  document.querySelector('#generate-button').addEventListener('click', onGenerateClick)
+}
+
+main();
+
